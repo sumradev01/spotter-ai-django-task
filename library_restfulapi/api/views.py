3051647542d3ced3b975805b5b35c4bb9a63b1c7
Sumra Khalid <sumradev01@gmail.com>
2024-08-22 16:33:29 +0500

    search by author name or book title

--- library_restfulapi/api/views.py
+++ library_restfulapi/api/views.py
@@ -63,6 +63,9 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['book_title', 'book_author__author_name']  # Fields to search in
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
